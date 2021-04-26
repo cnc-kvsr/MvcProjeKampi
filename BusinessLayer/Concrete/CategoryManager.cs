9b6b9454cef66c202e51bf4a0d3c054ac81db537
cnc-kvsr <kevsercanca@gmail.com>
2021-04-27 00:52:11 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category category)
+        {
+            if (category.CategoryName == "" || category.CategoryName.Length <= 3 ||
+                category.CategoryDescription == "" || category.CategoryName.Length >= 51)
+            {
+                //Hata mesajı
+            }
+            else
+            {
+                repo.Insert(category);
+            }
+        }
+    }
+}
